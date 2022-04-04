94dff2261369dbaa0918ac3fae9447c515b4362c
Neha Sharma <86730309+Neha120903@users.noreply.github.com>
2022-04-04 20:33:36 +0530

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,20 @@
+package BinaryTree;
+
+public class MainClass {
+	
+public static void main(String[] args) {
+	Binarytree obj= new Binarytree();
+	Node root=new Node(10);
+	root.left=new Node(20);
+	root.right=new Node(30);
+	root.left.left=new Node(40);
+	root.left.right=new Node(50);
+	root.right.left=new Node(60);
+	root.right.right=new Node(70);
+	//obj.inorderTraversal(root);
+	//obj.BFS(root);
+	obj.levelorder(root);
+	//System.out.print(obj.sizeOfBt(root));
+	System.out.print(obj.maximumOfbt(root));
+	}
+}
